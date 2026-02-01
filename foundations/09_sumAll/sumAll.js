35112780d389e721458e2b745481d401f05c09be
Fatema Althawadi <codebyfatem@gmail.com>
2026-02-02 00:22:43 +0300

    foundations exercise 9 solved

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function (range1, range2) {
+	let total = 0;
+    if((range1 < 0 
+        || range2 < 0 )
+        || !Number.isInteger(range1)
+        || !Number.isInteger(range2)){
+        return "ERROR";
+    }
+	if (range1 > range2) {
+        while(range1 != range2){
+            total += range1;
+            range1--;
+        }
+	} else {
+        while(range1 != range2){
+            total += range1;
+            range1++;
+        }
+	}
+    return total + range2;
 };
 
+//test
+console.log(sumAll(2, 3));
 // Do not edit below this line
 module.exports = sumAll;
