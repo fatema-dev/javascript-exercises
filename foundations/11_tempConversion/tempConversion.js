f06005a03bcfd1e8d24d56ef7ef958f1e2163b31
Fatema Althawadi <codebyfatem@gmail.com>
2026-02-02 21:05:05 +0300

    foundations exercise 11 solved

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,14 +1,14 @@
 const convertToCelsius = function(temprature) {
-  return Math.round((temprature-32)*(5/9));
+  return parseFloat(((temprature-32)*(5/9)).toFixed(1));
 };
 
 const convertToFahrenheit = function(temprature) {
-  return Math.round((temprature*(9/5)+32));
+  return parseFloat(((temprature*(9/5)+32)).toFixed(1));
 };
 
 //test
-console.log(convertToCelsius(32)); // fahrenheit to celsius, should return 0
-console.log(convertToFahrenheit(0)); // celsius to fahrenheit, should return 32
+console.log(convertToCelsius(100)); // fahrenheit to celsius, should return 0
+console.log(convertToFahrenheit(72)); // celsius to fahrenheit, should return 32
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
